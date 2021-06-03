2226805aac6b78dd3b167c9e230d38b58f3f91ea
Greg Connell <gconnell@160over90.com>
2021-06-02 18:22:20 -0700

    image-manager migration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import InlineSrc from './lib/inline-src.js'
+import * as ImageManager from './lib/image-manager.js'
 
 // expose the lib packages as modules
-export { InlineSrc }
+export { InlineSrc, ImageManager }
